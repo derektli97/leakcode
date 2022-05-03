af8390696a0a8056043a71b4ef8699ebf81b45ac
Derek <derek_t_li@tamu.edu>
2022-05-03 18:58:48 -0500

    first commit

---
+++ lc54_spiral_matrix.py
@@ -0,0 +1,36 @@
+matrix = [[1,2,3],[4,5,6],[7,8,9]]
+
+horizontal, vertical = 0
+
+ROWS, COLS = len(matrix), len(matrix[0])
+
+check_set = set()
+
+res = []
+
+# while ROWS or COLS:
+#     for i in range(ROWS-1):
+        
+        
+#     if horizontal == COLS:
+#         if [horizontal + 1, vertical] in check_set and [horizontal, vertical - 1] in check_set:
+#             return res
+#         else:
+#             horizontal += 1
+#             check_set.add([horizontal, vertical])
+                    
+            
+        
+for i in range(ROWS-1):
+    print('hi')
+for i in range(COLS-1,-1,-1):
+    print('hi')
+            
+
+
+
+for i in range(ROWS-1):
+    print('hi')
+for i in range(COLS-1,-1,-1):
+    print('hi')
+    
